2dc12f50cc7273c873ed8ff372663d57ccf2b8f3
Lounnaci-gi <ahmedlounnaci@gmail.com>
2026-02-17 14:23:03 +0100

    17022026+++

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { MongoClient } = require('mongodb');
 const cors = require('cors');
 const crypto = require('crypto');
-require('dotenv').config();
+require('dotenv').config({ path: '../.env' });
 
 const app = express();
 const PORT = process.env.PORT || 5000;
